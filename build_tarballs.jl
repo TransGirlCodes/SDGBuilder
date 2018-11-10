4dec39648aa94a64e32c1049081968a3b43e3c7e
Ben J. Ward <ward9250@gmail.com>
2018-11-10 22:27:38 +0000

    Update for libbsgjl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,59 +1,48 @@
-# Note that this script can accept some limited command-line arguments, run
-# `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-name = "BSGBuilder"
-version = v"0.1.0"
+name = "bsgjl"
+version = v"0.0.1"
 
-# Collection of sources required to build BSGBuilder
 sources = [
     "https://github.com/bioinfologics/bsg.git" =>
-    "68faa68692bbd3e32e697ddff13a7431988e9922",
-
+    "263f8b96d8bf170fe0763ed95111cf85b80d23b1",
 ]
 
-# Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/bsg
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain
+cd $WORKSPACE/srcdir
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DCMAKE_FIND_ROOT_PATH=${prefix} -DJulia_PREFIX=${prefix} -DBUILD_JULIA_INTERFACE=on ../bsg
 make
 make install
-
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc8)),
-    Linux(:aarch64, libc=:glibc, compiler_abi=CompilerABI(:gcc8)),
-    Linux(:powerpc64le, libc=:glibc, compiler_abi=CompilerABI(:gcc8)),
-    Linux(:x86_64, libc=:musl, compiler_abi=CompilerABI(:gcc8)),
-    Linux(:aarch64, libc=:musl, compiler_abi=CompilerABI(:gcc8)),
-    MacOS(:x86_64, compiler_abi=CompilerABI(:gcc8)),
-    FreeBSD(:x86_64, compiler_abi=CompilerABI(:gcc8))
-]
+platforms = Platform[]
+_abis(p) = (:gcc7,:gcc8)
+_archs(p) = (:x86_64,)
+
+for p in (Linux, MacOS)
+    for a in _archs(p)
+        for abi in _abis(p)
+            push!(platforms, p(a, compiler_abi=CompilerABI(abi,:cxx11)))
+        end
+    end
+end
 
 # The products that we will ensure are always built
 products(prefix) = [
-    ExecutableProduct(prefix, "bsg-flowmaker", Symbol("bsg-flowmaker")),
     LibraryProduct(prefix, "libbsg", :libbsg),
-    ExecutableProduct(prefix, "bsg-workspace", Symbol("bsg-workspace")),
-    ExecutableProduct(prefix, "runBasicTests", :runBasicTests),
-    ExecutableProduct(prefix, "seq-thread", Symbol("seq-thread")),
-    ExecutableProduct(prefix, "gfa-extract", Symbol("gfa-extract")),
-    ExecutableProduct(prefix, "bsg-untangler", Symbol("bsg-untangler")),
-    ExecutableProduct(prefix, "bsg-lhapassembler", Symbol("bsg-lhapassembler")),
-    ExecutableProduct(prefix, "bsg-kmerspectra", Symbol("bsg-kmerspectra")),
-    ExecutableProduct(prefix, "bsg-mapper", Symbol("bsg-mapper")),
-    ExecutableProduct(prefix, "bsg-datastore", Symbol("bsg-datastore"))
+    LibraryProduct(prefix, "libbsgjl", :libbsgjl)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
     "https://github.com/staticfloat/Bzip2Builder/releases/download/v1.0.6-1/build_Bzip2.v1.0.6.jl",
-    "https://github.com/BenJWard/ZLibBuilder/releases/download/v1.2.11/build_ZLibBuilder.v1.2.11.jl"
+    "https://github.com/BenJWard/ZLibBuilder/releases/download/v1.2.11/build_ZLibBuilder.v1.2.11.jl",
+    "https://github.com/JuliaInterop/libcxxwrap-julia/releases/download/v0.5.1/build_libcxxwrap-julia-1.0.v0.5.1.jl",
+    "https://github.com/JuliaPackaging/JuliaBuilder/releases/download/v1.0.0-2/build_Julia.v1.0.0.jl"
 ]
 
-# Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
-
